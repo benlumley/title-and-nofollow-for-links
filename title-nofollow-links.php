692d25f65b4f248ebd9d97214a25500c736aef0a
Flector <Flector@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-11 19:58:29 +0000

    first version
    git-svn-id: https://plugins.svn.wordpress.org/title-and-nofollow-for-links/trunk@1158185 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ title-nofollow-links.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Plugin Name: Title and Nofollow For Links
+Plugin URI: http://wordpress.org/plugins/title-nofollow-links/
+Description: The plugin adds a title and a rel="nofollow" checkbox to the insert link popup box.
+Version: 1.00
+Author: Flector
+Author URI: https://profiles.wordpress.org/flector#content-plugins
+License: GPLv2
+License URI: http://www.opensource.org/licenses/GPL-2.0
+*/ 
+
+function tnl_restore_title() {
+	wp_enqueue_script( 'wplinkTitle', plugins_url( '/inc/restore-title.js', __FILE__ ), array( 'wplink' ), '1.3', true );
+	wp_localize_script( 'wplinkTitle', 'wpLinkTitleL10n', array(
+		'titleLabel' => __( 'Title' ),
+	));
+}
+add_action('wp_enqueue_editor', 'tnl_restore_title', 20);
+
+function tnl_add_nofollow() {
+    wp_deregister_script('wplink');
+    wp_register_script('wplink',  plugins_url( '/inc/nofollow.min.js', __FILE__ ), array('jquery'), false, 1);
+    wp_localize_script('wplink', 'wpLinkL10n', array(
+        'title' => __('Insert/edit link'),
+        'update' => __('Update'),
+        'save' => __('Add Link'),
+        'noTitle' => __('(no title)'),
+        'noMatchesFound' => __('No results found.'),
+        'noFollow' => __(' Add <code>rel="nofollow"</code> to link', 'title-nofollow-links')
+    ));
+}
+add_action('wp_enqueue_editor', 'tnl_add_nofollow', 21);
+
+function title_nofollow_links_setup(){
+    load_plugin_textdomain('title-nofollow-links', null, dirname( plugin_basename( __FILE__ ) ) . '/lang' );
+}
+add_action('init', 'title_nofollow_links_setup');
+
+?>
